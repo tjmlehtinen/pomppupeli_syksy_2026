3ae98bec1ee5a8d26d61fcf80258f9e705bf98ff
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2026-09-07 19:32:29 +0300

    07092026

--- script.js
+++ script.js
@@ -2,6 +2,26 @@
 const pelinLeveys = 800;
 const pelinKorkeus = 600;
 
+const lautta = {
+    x: pelinLeveys / 2,
+    y: pelinKorkeus - 30,
+    leveys: 200,
+    korkeus: 50,
+    piirra: function() {
+        rectMode(CENTER);
+        fill(0, 255, 0);
+        rect(this.x, this.y, this.leveys, this.korkeus);
+    },
+    paivita: function() {
+        this.x = mouseX;
+    },
+    osuuko: function(x, y) {
+        const osuuX = this.x - this.leveys / 2 < x && x < this.x + this.leveys / 2;
+        const osuuY = this.y - this.korkeus / 2 < y && y < this.y + this.korkeus / 2;
+        return osuuX && osuuY;
+    }
+}
+
 class Pomppija {
     constructor(x, y, xMuutos, yMuutos) {
         this.x = x;
@@ -23,7 +43,7 @@ class Pomppija {
         this.y += this.yMuutos;
 
         // pomppu, jos osuu
-        if (this.y > pelinKorkeus) {
+        if (lautta.osuuko(this.x, this.y) && this.yMuutos > 0) {
             this.yMuutos = -1 * this.yMuutos;
         }
 
@@ -43,4 +63,7 @@ function draw() {
     background(100);
     testiPomppija.piirra();
     testiPomppija.paivita();
+
+    lautta.piirra();
+    lautta.paivita();
 }
