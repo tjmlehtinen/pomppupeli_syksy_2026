245e9ff9f04e681a7311e16762c108c685d47954
Tommi Lehtinen <tommijmlehtinen@gmail.com>
2026-09-14 19:31:26 +0300

    14092026

--- script.js
+++ script.js
@@ -41,29 +41,43 @@ class Pomppija {
         // liikkuminen
         this.x += this.xMuutos;
         this.y += this.yMuutos;
-
+        
         // pomppu, jos osuu
         if (lautta.osuuko(this.x, this.y) && this.yMuutos > 0) {
             this.yMuutos = -1 * this.yMuutos;
         }
-
+        
         // painovoiman vaikutus
-        this.yMuutos += 1;
+        this.yMuutos += 0.1;
     }
 }
 
-let testiPomppija = new Pomppija(100, 100, 0, 0);
+let pomppijat = [];
+function teePomppijoita() {
+    setInterval(function() {
+        let uusiPomppija = new Pomppija(100, 100, 3, 0);
+        pomppijat.push(uusiPomppija);
+        console.log("pomppijoita: " + pomppijat.length);
+    }, 1000);
+}
 
 function setup() {
     createCanvas(pelinLeveys, pelinKorkeus);
+    frameRate(30);
     background(100);
+    teePomppijoita();
 }
 
 function draw() {
     background(100);
-    testiPomppija.piirra();
-    testiPomppija.paivita();
 
     lautta.piirra();
     lautta.paivita();
+
+    for (let pomppija of pomppijat) {
+        pomppija.piirra();
+        pomppija.paivita();
+    }
+
+    pomppijat = pomppijat.filter((p) => (p.y <= pelinKorkeus));
 }
